336cd16d9b3aebd2489f3cc18e7c9fd4735cd07d
Polochon-street <polochonstreet@gmx.fr>
2015-09-09 08:24:27 +0200

    Began creating a proper menubar and splitting files

--- src/analyze.h
+++ src/analyze.h
@@ -48,6 +48,7 @@ struct song {
 float amp_sort(struct song);
 struct d2vector envelope_sort(struct song);
 int audio_decode(const char *file, struct song *);
-float lelele_analyze(char *filename, struct song *); 
+float analyze(char *filename, struct song *); 
 float freq_sort(struct song);
-void lelele_free_song(struct song *);
+int gui(int argc, char **argv);
+void free_song(struct song *);
